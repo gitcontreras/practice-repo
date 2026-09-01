f711eb9542b3e5e259ef11e29cb5267ffc67efc6
alex <oscar.contreras@dacodes.com.mx>
2026-09-01 14:20:51 -0600

    html

--- frontend/my-app/app/page.tsx
+++ frontend/my-app/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentation of this
           </a>
         </div>
       </main>
